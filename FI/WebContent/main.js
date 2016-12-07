30b227303c9f81827d011edb8d4f17f69ec575ed
Nikolai Gorokhov <nikolai@vaadin.com>
2016-12-07 22:21:12 +0200

    Communication between controllers

--- FI/WebContent/main.js
+++ FI/WebContent/main.js
@@ -1,32 +1,53 @@
+mc1 = new mc100();
+mc2 = new mc100();
 
+mc1.onValueChange(listenPortMc1);
+mc2.onValueChange(listenPortMc2);
 
-mc = new mc100();
-
-mc.onValueChange(listenPort);
-
-function setCommands(elem) {
+function setCommands(mc, elem) {
   mc.setProgram(elem.value);
 }
 
 function launch() {
-	setPortValue('p0', "p0");
-	setPortValue('p1', "p1");
-	mc.launch();
+	mc1.launch();
 }
 
 function step() {
-  mc.step();
+	setPortValue(mc1, 'p0', "p0");
+	setPortValue(mc1, 'p1', "p1");
+	setPortValue(mc2, 'p0', "p1");
+	setPortValue(mc2, 'p1', "p2");
+	mc1.step();
+	mc2.step();
 }
 
 function reset() {
-  mc.reset();
+  mc1.reset();
+  mc2.reset();
 }
 
-function listenPort(port, value) {
-	var input = document.querySelector("#"+port);
+function listenPortMc1(port, value) {
+	var id;
+	switch(port) {
+	case "p0": id="#p0"; break;
+	case "p1": id="#p1"; break;
+	}
+	
+	var input = document.querySelector(id);
 	input.value = value;
 }
 
-function setPortValue(port, elem) {
+function listenPortMc2(port, value) {
+	var id;
+	switch(port) {
+	case "p0": id="#p1"; break;
+	case "p1": id="#p2"; break;
+	}
+	
+	var input = document.querySelector(id);
+	input.value = value;
+}
+
+function setPortValue(mc, port, elem) {
 	mc.setPortValue(port, document.querySelector("#"+elem).value);
 }
